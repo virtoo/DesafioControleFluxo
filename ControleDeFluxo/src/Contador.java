a47c30625a0dd6938fb2dcbcdb713d7d91b86c57
virtoo <victorhfidelis@gmail.com>
2024-05-06 21:05:37 -0300

    primeiro commit

---
+++ ControleDeFluxo/src/Contador.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Contador {
+    public static void main(String[] args) {
+        Scanner terminal = new Scanner(System.in);
+
+        int parametroUm = terminal.nextInt();
+        int parametroDois = terminal.nextInt();
+
+    }
+    static void contar(int parametroUm, int parametroDois) throws ParametroInvalidoException{
+
+    }
+}
---
+++ ControleDeFluxo/src/ParametroInvalidoException.java
@@ -0,0 +1,2 @@
+public class ParametroInvalidoException extends Exception {
+}
